6de90ffe91ddaedf6eb72054a51f1bf720286724
Zaniar Shokati <zaniar.shokati@gmail.com>
2023-10-07 15:26:33 +0200

    get back viz.py

---
+++ viz.py
@@ -0,0 +1,103 @@
+import glob
+from sklearn.cluster import KMeans
+from utils import *
+
+INPUT_PATH = './Images/slice*.nii.gz'
+OUTPUT_PATH = './Vessels/'
+OVERLAY_PATH = './Vessel_overlayed/'
+paths = sorted(glob.glob(INPUT_PATH))
+my_file = open('vessel_volumes.csv', 'w')
+lung_areas_csv = []
+ratios = []
+
+
+def split_array_coords(array, idx=0, indy=1):
+    x = [array[i][idx] for i in range(len(array))]
+    y = [array[i][indy] for i in range(len(array))]
+    return x, y
+
+
+def create_vessel_mask(lung_mask, ct_numpy, denoise=False):
+    vessels = lung_mask * ct_numpy  # isolate lung area
+    vessels[vessels == 0] = -1000
+    vessels[vessels >= -500] = 1
+    vessels[vessels < -500] = 0
+    show_image_slice(vessels)
+    if denoise:
+        return denoise_vessels(lungs_contour, vessels)
+    show_image_slice(vessels)
+
+    return vessels
+
+
+for c, exam_path in enumerate(paths):
+    img_name = exam_path.split("/")[-1].split('.nii')[0]
+    vessel_name = OUTPUT_PATH + img_name + "_vessel_only_mask"
+    overlay_name = OVERLAY_PATH + img_name + "_vessels"
+
+    ct_img = nib.load(exam_path)
+    pixdim = extract_pixel_dimensions(ct_img)
+    ct_numpy = ct_img.get_fdata()
+
+    contours = segment_intensity(ct_numpy, -1000, -300)
+
+    lungs_contour = find_lung_contours(contours)
+    lung_mask = create_mask_from_polygon(ct_numpy, lungs_contour)
+
+    lung_area = compute_lung_area(lung_mask, extract_pixel_dimensions(ct_img))
+
+    vessels_only = create_vessel_mask(lung_mask, ct_numpy, denoise=True)
+
+    overlay_image_with_mask(ct_numpy, vessels_only)
+    plt.savefig(overlay_name)
+    plt.close()
+
+    save_nifty_binary_mask(vessels_only, vessel_name, affine_matrix=ct_img.affine)
+
+    vessel_area = compute_lung_area(vessels_only, extract_pixel_dimensions(ct_img))
+    ratio = (vessel_area / lung_area) * 100
+    print(img_name, 'Vessel %:', ratio)
+    lung_areas_csv.append([img_name, lung_area, vessel_area, ratio])
+    ratios.append(ratio)
+
+# Data viz here
+x, y = split_array_coords(lung_areas_csv, idx=1, indy=2)
+data2d = np.stack([np.asarray(x), np.asarray(y)], axis=1)
+
+kmeans = KMeans(init="random", n_clusters=2)
+kmeans.fit(data2d)
+print('Centers 2D:', kmeans.cluster_centers_)
+print('Slice labels:', kmeans.labels_)
+
+class1 = [data2d[i, :] for i in range(len(kmeans.labels_)) if kmeans.labels_[i] == 0]
+class2 = [data2d[i, :] for i in range(len(kmeans.labels_)) if kmeans.labels_[i] == 1]
+
+plt.figure()
+x, y = split_array_coords(class1)
+plt.scatter(x, y, c='r')
+x, y = split_array_coords(class2)
+plt.scatter(x, y, c='b')
+plt.title('K-means clustered data')
+plt.savefig('kmeans-clustered')
+
+plt.figure()
+x, _ = split_array_coords(lung_areas_csv, idx=3, indy=2)
+plt.scatter(x, np.arange(len(x)))
+plt.title('Ratios of different slices')
+plt.savefig('./Ratios')
+plt.close()
+
+# assign categories
+categories = np.zeros(len(x), dtype=int)
+
+for c, i in enumerate(x):
+    if i > 6:
+        categories[c] = int(1)
+
+colormap = np.array(['r', 'b'])
+
+plt.figure()
+plt.scatter(np.arange(len(x)) + 1, x, c=colormap[categories])
+plt.title('clustered Ratios')
+plt.savefig('./clustered_ratios')
+plt.close()
