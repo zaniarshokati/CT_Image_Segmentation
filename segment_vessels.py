cf91e72a753822b7c76e76d1b607d27616598533
Zaniar Shokati <zaniar.shokati@gmail.com>
2023-11-07 20:45:39 +0100

    change the structure

--- segment_vessels.py
+++ segment_vessels.py
@@ -5,17 +5,17 @@ import nibabel as nib
 import matplotlib.pyplot as plt
 from utils import *
 
-INPUT_PATH = "./Images/slice*.nii.gz"
-OUTPUT_PATH = "./Vessels/"
-OVERLAY_PATH = "./Vessel_overlayed/"
-OUTPUT_CSV_PATH = "vessel_volumes.csv"
+class VesselVolumeAnalyzer:
+    def __init__(self, input_path, output_path, overlay_path, output_csv_path):
+        self.input_path = input_path
+        self.output_path = output_path
+        self.overlay_path = overlay_path
+        self.output_csv_path = output_csv_path
 
-
-class Application:
     def process_image(self, exam_path):
-        img_name = exam_path.split("/")[-1].split(".nii")[0]
-        vessel_name = os.path.join(OUTPUT_PATH, f"{img_name}_vessel_only_mask")
-        overlay_name = os.path.join(OVERLAY_PATH, f"{img_name}_vessels")
+        img_name = os.path.basename(exam_path).split(".nii")[0]
+        vessel_name = os.path.join(self.output_path, f"{img_name}_vessel_only_mask")
+        overlay_name = os.path.join(self.overlay_path, f"{img_name}_vessels")
 
         ct_img = nib.load(exam_path)
         ct_numpy = ct_img.get_fdata()
@@ -26,9 +26,7 @@ class Application:
 
         lung_area = compute_lung_area(lung_mask, extract_pixel_dimensions(ct_img))
 
-        vessels_only = create_vessel_mask(
-            lung_mask, lungs_contour, ct_numpy, denoise=True
-        )
+        vessels_only = create_vessel_mask(lung_mask, lungs_contour, ct_numpy, denoise=True)
 
         overlay_image_with_mask(ct_numpy, vessels_only)
         plt.title("Overlayed plot")
@@ -39,29 +37,30 @@ class Application:
 
         vessel_area = compute_lung_area(vessels_only, extract_pixel_dimensions(ct_img))
         ratio = (vessel_area / lung_area) * 100
-        print(img_name, "Vessel %:", ratio)
+        print(f"{img_name} - Vessel %: {ratio:.2f}%")
 
         return [img_name, lung_area, vessel_area, ratio]
 
-    def main(self):
-        paths = sorted(glob.glob(INPUT_PATH))
-        create_directory(OUTPUT_PATH)
-        create_directory(OVERLAY_PATH)
+    def analyze_images(self):
+        paths = sorted(glob.glob(self.input_path))
+        create_directory(self.output_path)
+        create_directory(self.overlay_path)
 
-        lung_areas_csv = []
-        ratios = []
+        vessel_data = []
 
         for exam_path in paths:
             img_data = self.process_image(exam_path)
-            lung_areas_csv.append(img_data)
-            ratios.append(img_data[3])
+            vessel_data.append(img_data)
 
-        # Save data to csv file
-        with open(OUTPUT_CSV_PATH, "w", newline="") as my_file:
+        with open(self.output_csv_path, "w", newline="") as my_file:
             writer = csv.writer(my_file)
-            writer.writerows(lung_areas_csv)
-
+            writer.writerows(vessel_data)
 
 if __name__ == "__main__":
-    app = Application()
-    app.main()
+    INPUT_PATH = "./Images/slice*.nii.gz"
+    OUTPUT_PATH = "./Vessels/"
+    OVERLAY_PATH = "./Vessel_overlayed/"
+    OUTPUT_CSV_PATH = "vessel_volumes.csv"
+
+    analyzer = VesselVolumeAnalyzer(INPUT_PATH, OUTPUT_PATH, OVERLAY_PATH, OUTPUT_CSV_PATH)
+    analyzer.analyze_images()
